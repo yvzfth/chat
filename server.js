e5f3683e8c39fb9c3ae25870d78cef1c53801d40
Fatih Yavuz <yvzfth@yandex.com>
2023-01-10 12:58:45 +0300

    change app name

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = 'ChatCord Bot';
+const botName = 'ChatDev Bot';
 
 (async () => {
   pubClient = createClient({ url: 'redis://127.0.0.1:6379' });
@@ -39,7 +39,7 @@ io.on('connection', (socket) => {
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatDev!'));
 
     // Broadcast when a user connects
     socket.broadcast
